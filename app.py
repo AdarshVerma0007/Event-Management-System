cbba59928a574440516e78547e7fe86ab352dc96
Adarsh Verma <adarsh@example.com>
2025-06-25 08:57:39 +0530

    Updated code

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template,request,redirect,session,flash,url_for
+from db_config import get_db_connection
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+app.secret_key='your_secret_key'
+
+@app.route('/')
+def home():
+    return render_template("/template/index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
