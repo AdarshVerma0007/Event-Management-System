83c654d4f1243c721b4eeb95c495c2b982b45678
Adarsh Verma <adarsh@example.com>
2025-06-26 12:32:19 +0530

    Third Commit

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask, render_template,request,redirect,session,flash,url_for
+from flask import Flask, render_template, request, redirect, session, flash, url_for
 from db_config import get_db_connection
 from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.secret_key='your_secret_key'
+app.secret_key = '2024@darsh'
+
 
 @app.route('/')
 def home():
-    return render_template("/template/index.html")
+    return render_template('index.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
